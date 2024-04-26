14aaa9c636de743b1924a52e6a676c353ce342c4
Arda Macbook Pro <ardamacbookpro@192.168.1.100>
2024-04-26 12:07:53 +0300

    added read more and changed the photo

--- script.js
+++ script.js
@@ -7,7 +7,51 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+// Get the modal
+var modal = document.getElementById("readMoreModal");
 
+// Get the button that opens the modal
+var btn = document.querySelector(".about .btn-box .btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-btn")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function(event) {
+    event.preventDefault(); // Add this line
+    modal.style.display = "block";
+    setTimeout(() => modal.classList.add('show'), 10); // Add the show class after a short delay
+
+    // Initialize Swiper inside the modal when it's opened
+    new Swiper('.swiper-container.read-more', {
+        loop: false,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 10,
+        pagination: {
+            el: '.swiper-pagination.read-more',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next.read-more',
+            prevEl: '.swiper-button-prev.read-more',
+        },
+    });
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.classList.remove('show'); // Remove the show class
+    setTimeout(() => modal.style.display = 'none', 500); // Hide the modal after the transition
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.classList.remove('show'); // Remove the show class
+        setTimeout(() => modal.style.display = 'none', 500); // Hide the modal after the transition
+    }
+}
 
 // scroll sections
 let sections = document.querySelectorAll('section')
@@ -39,7 +83,7 @@ window.onscroll = () => {
     // sticky header
     let header = document.querySelector('header');
 
-    header.classList.toggle('sticky', window.scrollY > 100);
+    // header.classList.toggle('sticky', window.scrollY > 100);
 
     // remove toggle icon and navbar when click navbar links (scroll)
     menuIcon.classList.remove('bx-x');
